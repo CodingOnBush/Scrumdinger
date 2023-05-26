885d5514230087fd91ed471c2d599b58c1d62595
Mostafa <mostafa.omrane@proton.me>
2023-05-26 10:56:09 +0200

    "Handling errors" : DONE

---
+++ Scrumdinger/Models/ErrorWrapper.swift
@@ -0,0 +1,20 @@
+//
+//  ErrorWrapper.swift
+//  Scrumdinger
+//
+//  Created by VegaPunk on 26/05/2023.
+//
+
+import Foundation
+
+struct ErrorWrapper: Identifiable {
+    let id: UUID
+    let error: Error
+    let guidance: String
+    
+    init(id: UUID = UUID(), error: Error, guidance: String) {
+        self.id = id
+        self.error = error
+        self.guidance = guidance
+    }
+}
